23eba2c4de6db541afcb99aab688103ed685ef77
walte774 <walte774@morris.umn.edu>
2019-02-17 20:10:21 -0600

    Add a test to get a limited number of todos

---
+++ src/test/java/umm3601/todos/GetLimitedNumberOfTodosFromDB.java
@@ -0,0 +1,11 @@
+package umm3601.todos;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+
+public class GetLimitedNumberOfTodosFromDB {
+}
