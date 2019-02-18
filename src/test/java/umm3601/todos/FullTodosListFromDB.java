e08b7656bfa33e14615c6249f429808cc61eece5
walte774 <walte774@morris.umn.edu>
2019-02-17 19:16:39 -0600

    Add a test totalTodosCount to FullTodosListFromDB to count the correct amount of Todos. Add a test firstTodoInFullList to FullTodosListFromDB to check if the first Todo is correct.

--- src/test/java/umm3601/todos/FullTodosListFromDB.java
+++ src/test/java/umm3601/todos/FullTodosListFromDB.java
@@ -1,4 +1,29 @@
 package umm3601.todos;
 
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+
 public class FullTodosListFromDB {
+
+  @Test
+  public void totalTodosCount() throws IOException {
+    TodosDatabase db = new TodosDatabase("src/main/data/todos.json");
+    Todo[] allTodos = db.listTodos(new HashMap<>());
+    assertEquals("Incorrect total number of todos", 300, allTodos.length);
+  }
+
+  @Test
+  public void firstTodoInFullList() throws IOException {
+    TodosDatabase db = new TodosDatabase("src/main/data/todos.json");
+    Todo[] allTodos = db.listTodos(new HashMap<>());
+    Todo firstTodo = allTodos[0];
+    assertEquals("Incorrect owner", "Blanche", firstTodo.owner);
+    assertEquals("Incorrect status", false, firstTodo.status);
+    assertEquals("Incorrect body", "In sunt ex non tempor cillum commodo amet incididunt anim qui commodo quis. Cillum non labore ex sint esse.", firstTodo.body);
+    assertEquals("Incorrect category", "software design", firstTodo.category);
+  }
 }
