e81344917dced736af1c298a8f918fd40aa26d01
walte774 <walte774@morris.umn.edu>
2019-02-17 19:19:28 -0600

    Add a constructor TodosDatabase to TodosDatabase.

--- src/main/java/umm3601/todos/TodosDatabase.java
+++ src/main/java/umm3601/todos/TodosDatabase.java
@@ -2,10 +2,20 @@ package umm3601.todos;
 
 import java.util.Arrays;
 import java.util.Map;
+import com.google.gson.Gson;
+
+import java.io.FileReader;
+import java.io.IOException;
 
 public class TodosDatabase {
 
   private Todo[] allTodos;
+
+  public TodosDatabase(String todosDataFile) throws IOException {
+    Gson gson = new Gson();
+    FileReader reader = new FileReader(todosDataFile);
+    allTodos = gson.fromJson(reader, Todo[].class);
+  }
 
   public Todo getTodo(String id) {
     return Arrays.stream(allTodos).filter(x -> x._id.equals(id)).findFirst().orElse(null);
