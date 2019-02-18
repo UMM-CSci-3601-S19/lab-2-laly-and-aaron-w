6ae1c4e61e78ca07b5905d075f34e1dc5dc72efe
walte774 <walte774@morris.umn.edu>
2019-02-17 18:49:30 -0600

    Add getAllTodos to todos.js.
    Add a function HttpClient to todos.js also.

---
+++ src/main/resources/public/javascript/todos.js
@@ -0,0 +1,36 @@
+
+
+function getAllTodos() {
+  console.log("Getting all the todos.");
+
+  var HttpThingy = new HttpClient();
+  HttpThingy.get("/api/todos", function (returned_json) {
+    document.getElementById('jsonDump').innerHTML = returned_json;
+  });
+}
+
+function HttpClient() {
+  // We'll take a URL string, and a callback function.
+  this.get = function (aUrl, aCallback) {
+    var anHttpRequest = new XMLHttpRequest();
+
+    // Set a callback to be called when the ready state of our request changes.
+    anHttpRequest.onreadystatechange = function () {
+
+      /**
+       * Only call our 'aCallback' function if the ready state is 'DONE' and
+       * the request status is 200 ('OK')
+       *
+       * See https://httpstatuses.com/ for HTTP status codes
+       * See https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/readyState
+       *  for XMLHttpRequest ready state documentation.
+       *
+       */
+      if (anHttpRequest.readyState === 4 && anHttpRequest.status === 200)
+        aCallback(anHttpRequest.responseText);
+    };
+
+    anHttpRequest.open("GET", aUrl, true);
+    anHttpRequest.send(null);
+  }
+}
