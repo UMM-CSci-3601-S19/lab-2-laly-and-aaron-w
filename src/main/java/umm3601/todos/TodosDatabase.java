f98662a57b81d13f051eec63491302b48fe7c05c
walte774 <walte774@morris.umn.edu>
2019-02-18 23:03:15 -0600

    Add server-side support for filtering by owner and category
      Add function getTodosByOwner in TodosDatabase
      Add function getTodosByCategory in TodosDatabase
      Add logic in listTodos to call the appropriate functions
    
      Relates to issues #13 #15 #16 #18

--- src/main/java/umm3601/todos/TodosDatabase.java
+++ src/main/java/umm3601/todos/TodosDatabase.java
@@ -32,6 +32,12 @@ public class TodosDatabase {
     if (queryParams.containsKey("contains")) {
       filteredTodos = getTodosByBodyString(filteredTodos, queryParams.get("contains")[0]);
     }
+    if (queryParams.containsKey("owner")) {
+      filteredTodos = getTodosByOwner(filteredTodos, queryParams.get("owner")[0]);
+    }
+    if (queryParams.containsKey("category")) {
+      filteredTodos = getTodosByCategory(filteredTodos, queryParams.get("category")[0]);
+    }
     if (queryParams.containsKey("limit")) {
       long limit = Long.parseLong(queryParams.get("limit")[0]);
       filteredTodos = getTodosByLimit(filteredTodos, limit);
@@ -47,6 +53,14 @@ public class TodosDatabase {
     return Arrays.stream(todos).filter(x -> x.body.contains(search)).toArray(Todo[]::new);
   }
 
+  public Todo[] getTodosByOwner(Todo[] todos, String owner) {
+    return Arrays.stream(todos).filter(x -> x.owner.equals(owner)).toArray(Todo[]::new);
+  }
+
+  public Todo[] getTodosByCategory(Todo[] todos, String category) {
+    return Arrays.stream(todos).filter(x -> x.category.equals(category)).toArray(Todo[]::new);
+  }
+
   public Todo[] getCompleteTodos(Todo[] todos) {
     return Arrays.stream(todos).filter(x -> x.status).toArray(Todo[]::new);
   }
@@ -58,9 +72,9 @@ public class TodosDatabase {
   public Todo[] getTodosByStatus(Todo[] todos, boolean status) {
     if (status) {
       return Arrays.stream(todos).filter(x -> x.status).toArray(Todo[]::new);
-    } else if (!status) {
+    } else {
       return Arrays.stream(todos).filter(x -> !x.status).toArray(Todo[]::new);
-    } else return todos;
+    }
   }
 
 }
